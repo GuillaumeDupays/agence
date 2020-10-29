b0e5eda1a40eae6c9037e833cc817b08d1aa2b8b
GuillaumeDupays <guillaumedupays@gmail.com>
2020-10-29 14:03:37 +0100

    boucles savoir-faire

--- agence-front/src/app/app-routing.module.ts
+++ agence-front/src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import {SavoirFaireComponent} from './savoir-faire/savoir-faire.component';
 import {ContactComponent} from './contact/contact.component';
 import {BioComponent} from './bio/bio.component';
 import {PixelComponent} from './pixel/pixel.component';
+import {AdminComponent} from './admin/admin.component';
 
 const route: Routes = [
   { path: '', component: BioComponent },
   { path: 'bio', component: BioComponent },
   { path: 'savoir-faire', component: SavoirFaireComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'adminBlob', component: AdminComponent }
 ];
 
 @NgModule({
--- agence-front/src/app/savoir-faire/savoir-faire.component.ts
+++ agence-front/src/app/savoir-faire/savoir-faire.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./savoir-faire.component.scss']
 })
 export class SavoirFaireComponent implements OnInit {
+  frameworks = [ 'Angular', 'Vue.js <- acquisition en cours', 'React.js <- pour 2021', 'Node.js', 'Strapi'];
+  langages = [ 'Javascript/Typescript', 'Java <- en cours d\'acquisition', 'Html/Css' ];
+  competences = [ 'Concevoir et développer une application, un site web', 'Intégrer une maquette graphique', 'Réaliser l\'ensemble de la partie graphique d\'une interface web et mobile' ];
+  qualites = [ 'Curiosité', 'Créativité', 'Précision' ];
+  savoirEtre = [ 'Sens du service', 'Ecoute active', 'Pédagogie' ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
