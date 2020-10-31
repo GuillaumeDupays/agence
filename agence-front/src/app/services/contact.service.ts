e5ebdcf685d91a5386490b14db923478f6b0f854
GuillaumeDupays <guillaumedupays@gmail.com>
2020-10-31 16:39:56 +0100

    url api

--- agence-front/src/app/services/contact.service.ts
+++ agence-front/src/app/services/contact.service.ts
@@ -12,14 +12,14 @@ export class ContactService {
   constructor(private http: HttpClient) { }
 
   postMessageContact(msgContact: string) {
-    return this.http.post<MessageContact>('http://localhost:3000/api/v1/msg-contacts', msgContact);
+    return this.http.post<MessageContact>('http://admin.blob-zone.com/api/v1/msg-contacts', msgContact);
   }
 
   getMessageContact() {
-    return this.http.get('http://localhost:3000/api/v1/msg-contacts');
+    return this.http.get('http://admin.blob-zone.com/api/v1/msg-contacts');
   }
 
   deleteMsgById(id: string) {
-    return this.http.delete(`http://localhost:3000/api/v1/msg-contacts/${id}`);
+    return this.http.delete(`http://admin.blob-zone.com/api/v1/msg-contacts/${id}`);
   }
 }
