d96e8cd9de608786687fef7e9201c46d840641ce
GuillaumeDupays <guillaumedupays@gmail.com>
2020-11-03 12:21:51 +0100

    url services

--- agence-front/src/app/services/article.service.ts
+++ agence-front/src/app/services/article.service.ts
@@ -8,27 +8,27 @@ import {Tag} from '../models/tag';
 })
 export class ArticleService {
   private baseUrl = 'http://localhost:3000/api/v1';
-  private url: 'http://admin.blob-zone.com/api/v1';
+  private url: 'http://admin.blob-zone.com/api/v1/articles';
 
   constructor(private http: HttpClient) { }
 
   postArticle(article: string) {
-    return this.http.post<Article>(`${this.baseUrl}/articles`, article);
+    return this.http.post<Article>('http://admin.blob-zone.com/api/v1/articles', article);
   }
 
   postTag(tag: string) {
-    return this.http.post<Tag>(`${this.baseUrl}/tags`, tag);
+    return this.http.post<Tag>('http://admin.blob-zone.com/api/v1/tags', tag);
   }
 
   getTags() {
-    return this.http.get(`${this.baseUrl}/tags`);
+    return this.http.get('http://admin.blob-zone.com/api/v1/articles');
   }
 
   getArticles() {
-    return this.http.get(`${this.baseUrl}/articles`);
+    return this.http.get('http://admin.blob-zone.com/api/v1/articles');
   }
 
   deleteArticleById(id: string) {
-    return this.http.delete(`http://localhost:3000/api/v1/articles/${id}`);
+    return this.http.delete(`http://admin.blob-zone.com/api/v1/articles/${id}`);
   }
 }
